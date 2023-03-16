b29a623f5eb1d065d1ceebe260c736a0b323be62
John Russo <jdrusso@users.noreply.github.com>
2023-03-16 13:45:48 -0700

    Updated example for SLURM+Singularity

--- entrypoint-head.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SERVER_INFO="/data/west_zmq_info.json"
-
-cd /data
-w_run --work-manager=zmq --n-workers=0 --zmq-mode=master --zmq-write-host-info=$SERVER_INFO --zmq-comm-mode=tcp &> west.log 
-
-wait
